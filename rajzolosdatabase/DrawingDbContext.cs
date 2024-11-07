5ba848b7032d66859d1778bbdc087007f3282d3c
Mészáros Kristóf <123268805+Bubborek@users.noreply.github.com>
2024-11-07 12:14:24 +0100

    Add files via upload

---
+++ rajzolosdatabase/DrawingDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Drawing;
+
+public class DrawingDbContext : DbContext
+{
+    public DbSet<Drawing> Drawings { get; set; }
+    public DbSet<Point> Points { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=DrawingDB;Trusted_Connection=True;");
+    }
+}
