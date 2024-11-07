10dae21d686384b257268af398dd92781eacdc0d
Mészáros Kristóf <123268805+Bubborek@users.noreply.github.com>
2024-11-07 12:13:50 +0100

    Add files via upload

---
+++ Drawing.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+public class Drawing
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Point> Points { get; set; } = new List<Point>();
+}
---
+++ Point.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Point
+{
+    public int Id { get; set; }
+    public int DrawingId { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public char Character { get; set; }
+    public ConsoleColor Color { get; set; }
+
+    public Drawing Drawing { get; set; }
+}
